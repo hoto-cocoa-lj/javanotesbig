196bf5aebce7c3a0e11361edc1b79d0b327a2e1f
17181370591 <17181370591@sina.cn>
2018-10-24 19:04:34 +0800

    20181024

--- java待上传github代码/08-Hibernate框架/代码/hibernate_04/src/cn/itcast/domain/Linkman.java
+++ java待上传github代码/08-Hibernate框架/代码/hibernate_04/src/cn/itcast/domain/Linkman.java
@@ -47,7 +47,7 @@ public class Linkman
 	  @ManyToOne(targetEntity=Customer.class)
 	  // 维护外键关系
 	  /*name:外键字段名
-	  referencedColumnName:指向的主键字段名*/
+	  referencedColumnName:指向的主键字段名，这里外键名字叫wj_id，其实完全可以起名customer，表示该数据的主键的id*/
 	  @JoinColumn(name="wj_id",referencedColumnName="cust_id")
 	  private Customer customer;
 
